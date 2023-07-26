4f4a101618fd75eec61f0a308877864ebc259dd9
Yaxyobek03 <yaxyobekbahromjonov44@gmail.com>
2023-07-26 10:29:03 +0500

    add xizmatlar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from "react";
 import Header from "./components/Header";
-import Hero from "./components/Hero"
+import Hero from "./components/Hero";
 import Xizmatlar from "./components/Xizmatlar";
+import Cta from "./components/Cta";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      <Xizmatlar/>
+      <Xizmatlar />
+      <Cta />
     </>
   );
 }
---
+++ src/components/Cta.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Cta = () => {
+  return (
+    <div className='bg-[#0EB4C1]'>
+      <div className='xizmatlar-container'>
+        <p className="pt-16 text-center text-white text-4xl font-bold leading-[43.20px]">Qabulga ro’yxatdan o’ting</p>
+
+        <div className="  max-w-[194px] px-6 py-3 rounded-lg border border-white ">
+          <button className="text-white text-[15px] font-bold  leading-[18px] tracking-widest">Qo’ng’iroq qilish</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cta
