3d0f4d65f4ca377e961474ec4add3fd875f66d1b
Yaxyobek03 <yaxyobekbahromjonov44@gmail.com>
2023-07-26 10:50:06 +0500

    add Cta

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -3,12 +3,17 @@ import React from 'react'
 const Cta = () => {
   return (
     <div className='bg-[#0EB4C1]'>
-      <div className='xizmatlar-container'>
-        <p className="pt-16 text-center text-white text-4xl font-bold leading-[43.20px]">Qabulga ro’yxatdan o’ting</p>
+      <div className='xizmatlar-container py-16 '>
+        <p className="text-center text-white text-3xl font-bold leading-[43.20px] mb-10">Qabulga ro’yxatdan o’ting</p>
+        <div className='flex justify-center'>
+          <button className='bg-transparent border-2 border-white py-3 px-6 rounded-lg duration-500 text-white font-bold mr-10 hover:border-transparent hover:bg-[#DE2E54]
+'>QO'NG'IROQ QILISH</button>
 
-        <div className="  max-w-[194px] px-6 py-3 rounded-lg border border-white ">
-          <button className="text-white text-[15px] font-bold  leading-[18px] tracking-widest">Qo’ng’iroq qilish</button>
+          <button className='bg-transparent border-2 border-white py-3 px-6 rounded-lg duration-500 text-white font-bold uppercase hover:border-transparent hover:bg-[#DE2E54]
+'>Telegramdan yozish</button>
+
         </div>
+
       </div>
     </div>
   )
